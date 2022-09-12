bf95daf2005e7674c013ff84e7689986bb908c79
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-09-12 18:22:19 -0300

    Function

--- MeusExUri/AreaDoCirculo/index.js
+++ MeusExUri/AreaDoCirculo/index.js
@@ -4,15 +4,23 @@
 
 // console.log(AreaDoCirculo)
 
+//Função com retorno
 function AreaDoCirculo(PI = 0, raio) {
     return PI * raio * 2; 
 }
 
-console.log(AreaDoCirculo(3.16, 2.0))
+console.log(AreaDoCirculo(3.14, 2.0))
 
-
+//Função Anônima
 const AreaDoCirculoAnonima = function(PI, raio) {
     console.log(PI * raio * 2)
 }
 
-AreaDoCirculoAnonima(3.16, 2.0)
+AreaDoCirculoAnonima(3.14, 2.0)
+
+//Arrow Function 
+const AreaDoCirculoArrow = (PI, raio) => {
+    return PI * raio * 2
+}
+
+console.log(AreaDoCirculoArrow(3.14, 2.0))
